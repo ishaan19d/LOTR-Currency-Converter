56b95f1cad0c0ac452f1c5d79672eda910e92a36
ishaan19d <ishaandas848@gmail.com>
2024-06-05 19:09:49 +0530

    Till Stack and Views.

--- LOTR Converter/ContentView.swift
+++ LOTR Converter/ContentView.swift
@@ -9,13 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height:250)
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                // Conversion Section
+                HStack{
+                    VStack{
+                        HStack{
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        
+                        Text("Textfield")
+                            .foregroundStyle(.cyan)
+                    }
+                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                    
+                    // Right Conversion Section
+                    VStack{
+                        //currency
+                        HStack{
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        
+                        Text("Textfield")
+                            .foregroundStyle(.cyan)
+                    }
+                }
+                Spacer()
+                // Info button
+                //not completed tho
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+            }
         }
-        .padding()
     }
 }
 
