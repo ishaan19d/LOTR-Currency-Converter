a1e89d6122c52a11e2ceb92a57725c544df7710d
ishaan19d <ishaandas848@gmail.com>
2024-06-06 16:26:26 +0530

    error correction: "tracking 3"

--- LOTR Converter/ExchangeInfo.swift
+++ LOTR Converter/ExchangeInfo.swift
@@ -17,7 +17,7 @@ struct ExchangeInfo: View {
             VStack{
                 Text("Exchange Rates")
                     .font(.largeTitle)
-                    .tracking(7)
+                    .tracking(3)
                 
                 Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
                     .font(.title2)
