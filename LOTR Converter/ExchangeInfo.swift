0168ee3c1257d4dcce5767915bef58a2de090e0a
ishaan19d <ishaandas848@gmail.com>
2024-06-06 15:50:55 +0530

    Added Exchange info screen

---
+++ LOTR Converter/ExchangeInfo.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeInfo.swift
+//  LOTR Converter
+//
+//  Created by Ishaan Das on 06/06/24.
+//
+
+import SwiftUI
+
+struct ExchangeInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeInfo()
+}
